2a4e5f9bff5fa5041d16a04a20c88e23521a82d3
janroyvince-lasala <janroyvince.lasala@my.jru.edu>
2026-04-25 16:41:31 +0800

    Add secure Flask API microservice

---
+++ app/main.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/api', methods=['GET'])
+def secure_api():
+    return {'message': 'This is a secure Flask API'}, 200
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, ssl_context=('cert.pem', 'key.pem'))
